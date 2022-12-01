23b4b36145e5bf53753076552c34af3a1459dbb2
ravibabuvadde <79750913+ravibabuvadde@users.noreply.github.com>
2022-12-01 21:42:52 +0530

    Update: IST TimeZone

--- contests/index.js
+++ contests/index.js
@@ -13,8 +13,8 @@ const parseCodechef = (data) => {
         contest.site = 'codechef';
         contest.title = data[i].contest_name;
         contest.startTime = data[i].contest_start_date;
-        let date = new Date(contest.startTime);
-        let milliseconds = date.getTime();
+        let date = new Date(new Date(contest.startTime).toLocaleString("en-US", { timeZone: "IST" }))
+        let milliseconds = date.getTime()
         contest.startTime = milliseconds;
         contest.duration = data[i].contest_duration*60*1000;
         contest.endTime = contest.startTime + contest.duration;
